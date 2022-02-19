b6c5e12093d903a0418ac209eb77a331bab43a84
darshan4903 <123greatdarshan@gmail.com>
2022-02-19 14:34:32 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Routes, Route, Switch } from "react-router-dom";
+import {BrowserRouter} from 'react-router-dom';
 import './App.css';
+import Navbar from './Components/Navbar/Navbar'
+import Team from './Components/Team/Team';
+import Gallery from './Components/Gallery/Gallery';
+import Home from './Components/Home/Home';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+
+    return (
+        <div className="App">
+        <BrowserRouter>
+          <Switch>
+            <Navbar/>
+            <Route exact path='/'>
+              <Home/>
+             </Route>
+       
+
+           <Route exact path='/gallery'>
+             <Gallery/>
+
+           </Route>
+           <Route exact path='/team'>
+             <Team/>
+
+            </Route>
+            </Switch>
+            </BrowserRouter>
+</div>   
+    );
 }
 
 export default App;
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+    <nav class="uk-navbar-container" uk-navbar>
+    <div class="uk-navbar-left">
+        <ul class="uk-navbar-nav">
+            <li class="uk-active"><a href=""></a></li>
+            <li class="uk-parent"><a href=""></a></li>
+            <li><a href=""></a></li>
+        </ul>
+    </div>
+</nav>
+    </>
+  )
+}
+
+export default Navbar
