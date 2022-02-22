5729918d9a6e43304cf228ce8546aebc468f98c3
pnmsafi <pnmsafi@gmail.com>
2022-02-22 14:06:06 +0530

    nav

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,19 +1,34 @@
-import React from 'react'
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
 
 const Navbar = () => {
   return (
     <>
-    <nav class="uk-navbar-container" uk-navbar>
-    <div class="uk-navbar-left">
-        <ul class="uk-navbar-nav">
-            <li class="uk-active"><a href=""></a></li>
-            <li class="uk-parent"><a href=""></a></li>
-            <li><a href=""></a></li>
-        </ul>
-    </div>
-</nav>
-    </>
-  )
-}
+      <nav className="navbar px-4 py-4 bg-black text-zinc-100 flex justify-between items-center w-screen">
+        
+        <img src="\logo\logoexpresso.svg" alt="img" className="w-11 mx-4" />
+        <div className="nav">
+        <div className="hamburger inline-block p-4 md:hidden ">
+          <div className="line h-1 w-7 bg-black my-1"></div>
+          <div className="line h-1 w-7 bg-black my-1"></div>
+          <div className="line h-1 w-7 bg-black my-1"></div>
+          
+            </div>
 
-export default Navbar
+          <ul className="flex justify-end  " >
+            <li className="mx-4 "><a href="#">HOME</a></li>
+            <li className="mx-4 "><a href="#">EVENTS</a></li>
+            <li className="mx-4 "><a href="#">GALLERY</a></li>
+            <li className="mx-4 "><a href="#">ART</a></li>
+            <li className="mx-4 "><a href="#">LITRATURE</a></li>
+            <li className="mx-4 "><a href="#">TEAM</a></li>
+            <li className="mx-4 "><a href="#">ABOUT US</a></li>
+           
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    
+      './pages/**/*.{html,js}',
+      './components/**/*.{html,js}',
+      './index.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
